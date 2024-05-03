d722b3ce204a93570366656bc676534c374aed8e
Burak Orkmez <burakorkmezz@gmail.com>
2024-05-04 02:14:57 +0300

    BASE_URL bug fixed

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -2,6 +2,7 @@ import { Flex, Spinner, Stack, Text } from "@chakra-ui/react";
 
 import TodoItem from "./TodoItem";
 import { useQuery } from "@tanstack/react-query";
+import { BASE_URL } from "../App";
 
 export type Todo = {
 	_id: number;
@@ -14,7 +15,7 @@ const TodoList = () => {
 		queryKey: ["todos"],
 		queryFn: async () => {
 			try {
-				const res = await fetch("http://localhost:5000/api/todos");
+				const res = await fetch(BASE_URL + "/todos");
 				const data = await res.json();
 
 				if (!res.ok) {
